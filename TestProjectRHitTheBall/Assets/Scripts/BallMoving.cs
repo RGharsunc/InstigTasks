2f3daddd755cfbdbc50e8e4db34cf7ddd60b77ff
Rafayel <r.gharsunc@gmail.com>
2018-03-07 11:19:43 +0400

    - Initial commit

---
+++ TestProjectRHitTheBall/Assets/Scripts/BallMoving.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMoving : MonoBehaviour
+{
+
+    float speed = 10f;
+    float rotateSpeed = 100f;
+    private float thrust = 8f;
+    private Rigidbody rb;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+
+
+    void Update()
+    {
+
+        //if(Input.GetMouseButtonDown(0))
+        //{
+        //}
+        //transform.Translate(new Vector3(0, 0, Input.GetAxis("Vertical")*speed*Time.deltaTime));
+        //transform.Translate(new Vector3(0, Input.GetAxis("Mouse x") * rotateSpeed * Time.deltaTime, 0));
+
+        //if (Input.GetMouseButton(0))
+        //{
+        //    //rb.AddForce(transform.forward * thrust);
+            
+        //}
+
+        if (Input.GetMouseButton(0))
+        {
+            
+            rb.AddForce(Vector3.forward * thrust);
+        }
+    }
+}
